008a761075fc26d30731f1633f215bbe66316bde
James Kong <104340270+jameskongchief@users.noreply.github.com>
2022-09-15 16:51:51 +0800

    Added Webview

--- chiefx/src/main/java/hk/com/chiefgroup/chiefx/journeys/explorecenter/views/ExploreCenterItemDetailView.kt
+++ chiefx/src/main/java/hk/com/chiefgroup/chiefx/journeys/explorecenter/views/ExploreCenterItemDetailView.kt
@@ -1,19 +1,14 @@
 package hk.com.chiefgroup.chiefx.journeys.explorecenter.views
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.*
-import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
 import hk.com.chiefgroup.chiefx.journeys.explorecenter.datatypes.ExploreItem
 import hk.com.chiefgroup.chiefx.module.core.baseclasses.Dispatcher
 
@@ -35,7 +30,15 @@ fun ExploreCenterItemDetailView(item: ExploreItem?, dispatcher: Dispatcher) {
         },
         content = { padding ->
             Column(Modifier.padding(padding)) {
-                Text(text = item?.Title ?: "")
+
+                CustomWebView(
+                    modifier = Modifier,
+                    url = item?.WebsiteUrl ?: "https://google.com",
+                    onBack = { },
+                    onProgressChange = { },
+                    initSettings = {},
+                    onReceivedError = {}
+                )
             }
         }
     )
